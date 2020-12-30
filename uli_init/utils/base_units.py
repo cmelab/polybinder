c140e2e9d01553e6cec8043d1c3146c3af060bf5
chrisjonesBSU <chrisjones4@u.bosiestate.edu>
2020-12-29 21:10:30 -0700

    removed the fixed reference unit values from base_units

--- uli_init/utils/base_units.py
+++ uli_init/utils/base_units.py
@@ -8,10 +8,7 @@ def base_units():
     units['cm_to_nm'] = 1e7
     units["nm_to_m"] = 1e-9
     units["m_to_nm"] = 1e9
-    units["mass"] = 32.06
     units["mass_units"] = "amu"
-    units["energy"] = 1.046
     units["energy_units"] = "kj/mol"
-    units["distance"] = 0.35635948725613575
     units["distance_units"] = "nm"
     return units
