c554896efe4b9e9903f3232878c3ca4094471d7b
chrisjonesBSU <chrisjones4@u.bosiestate.edu>
2021-03-03 09:49:48 -0700

    fixed energy value in unit conversion

--- uli_init/utils/unit_conversions.py
+++ uli_init/utils/unit_conversions.py
@@ -11,7 +11,7 @@ def reduce_from_kelvin(T_SI, ref_energy, precision=2):
 
 def kelvin_from_reduced(T_reduced, ref_energy, precision=0):
     units = base_units.base_units()
-    T_SI = (T_reduced * ref_energy * units["kj_to_j"]) / (
+    T_SI = (T_reduced * ref_energy * units["kcal_to_j"]) / (
         units["avogadro"] * units["boltzmann"]
     )
     T_SI = round(T_SI, precision)
@@ -21,8 +21,8 @@ def kelvin_from_reduced(T_reduced, ref_energy, precision=0):
 def convert_to_real_time(dt, ref_energy, ref_distance, ref_mass, precision=3):
     units = base_units.base_units()
     time_tau = ref_mass * units["amu_to_kg"]
-    time_tau *= (ref_distance * units["nm_to_m"]) ** 2
-    time_tau /= ref_energy * units["kj_to_j"] / units["avogadro"]
+    time_tau *= (ref_distance * units["ang_to_m"]) ** 2
+    time_tau /= ref_energy * units["kcal_to_j"] / units["avogadro"]
     real_time = dt * (time_tau ** 0.5) * 1e15
     real_time = round(real_time, precision)
     return real_time
