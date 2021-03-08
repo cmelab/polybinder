f25f9af513a809f6d0bec57a5d7a9246cabd2eb6
RainierBarrett <rainier.barrett@gmail.com>
2021-03-08 15:37:23 -0700

    Add online demo file. Offline demo broken

--- Notebooks/HTF_Online_CG_Demo.py
+++ Notebooks/HTF_Online_CG_Demo.py
@@ -1,5 +1,5 @@
 import os
-#os.environ['CUDA_VISIBLE_DEVICES'] =  '-1'
+os.environ['CUDA_VISIBLE_DEVICES'] =  '-1'
 import networkx as nx
 import tensorflow as tf
 from tensorflow.keras import layers
@@ -8,23 +8,17 @@ import hoomd.md
 import hoomd.htf as htf
 import numpy as np
 import gsd
-import gsd.hoomd
-import pickle
 import matplotlib.pyplot as plt
 import os
+from uli_init.utils.smiles_utils import viz
+import uli_init.simulate as simulate
 
-# building a HTF model for coarse graining
-# here use the single-molecule file for simplicity
-# TODO: break this file up into a few scripts
-# e.g. one for making mapping, one for running from traj...
-# TODO: once saving is set, get pipeline for starting a sim fresh with uli-init
-# TODO: either calculate per-bead forces, or try just matching energies
+# building an HTF model for coarse graining
 
 def get_mol_mapping_idx(filename):
     '''Takes a filename of a .gsd file WITHOUT the '.gsd', loads that gsd,
     then loads or creates a molecule mapping index from it.'''
     # if the mapping doesn't exist, make it
-    gsdfile = gsd.hoomd.open(f'{filename}.gsd')
     context = hoomd.context.initialize('--mode=cpu')
     system = hoomd.init.read_gsd(filename=f'{filename}.gsd')
     context.sorter.disable()
@@ -36,6 +30,14 @@ def get_mol_mapping_idx(filename):
         molecule_mapping_index = np.load(f'{filename}-mapping.npy')
     return system, molecule_mapping_index
 
+# Steps to do HOOMD-TF:
+# 1) build HOOMD-TF keras model
+# 2) compile keras model
+# 3) create tfcompute object from model
+# 4) set up HOOMD simulation (uli-init code)
+# 5) pass in model to uli-init Simulation object
+# 7) run the hoomd simulation (call the quench method)
+
 one_molecule_fname = '1-length-4-peek-para-only'
 system, molecule_mapping_index = get_mol_mapping_idx(one_molecule_fname)
 
@@ -45,10 +47,6 @@ for particle in system.particles:
     graph.add_node(particle.tag, name=particle.type)
 for bond in system.bonds:
     graph.add_edge(bond.a, bond.b)
-plt.figure()
-plt.title('BEFORE')
-nx.draw(graph, with_labels=True)
-plt.savefig('before.png')
 # judiciously snip bonds
 degrees_dict = dict(graph.degree())
 
@@ -63,10 +61,6 @@ for i, bond in enumerate(system.bonds):
     degrees_dict = dict(graph.degree())
 
 subgraph_list = list(nx.connected_components(graph))
-plt.figure()
-plt.title('AFTER')
-nx.draw(graph, with_labels=True)
-plt.savefig('after.png')
 
 # now we have our beads grouped up, we need to get their mapping
 # get total N atoms
@@ -100,7 +94,7 @@ assert(np.sum(mapping_arr) == MN)
 
 bead_number = mapping_arr.shape[0]
 
-set_rcut = 11.0
+set_rcut = 7.0
 fname = '100-length-4-peek-para-only-production'
 system, molecule_mapping_index = get_mol_mapping_idx(fname)
 
@@ -113,9 +107,6 @@ M = len(molecule_mapping_index)
 MN = len(molecule_mapping_index[0])
 print('N_atoms:', N,'\nN_molecules:', M,'\nN_atoms_per_molecule:', MN)
 assert cg_mapping.shape == (M * bead_number, N)
-
-import MDAnalysis as mda
-univ = mda.Universe('100-length-4-peek-para-only-production.gsd')
 
 # create an edge list
 beads_per_molecule = 12
@@ -206,11 +197,11 @@ class DihedralLayer(tf.keras.layers.Layer):
         return energy
 
 class TrajModel(htf.SimModel):
-    def setup(self, cg_num, adjacency_matrix, CG_NN, cg_mapping, rcut):
+    def setup(self, cg_num, adjacency_matrix, CG_NN, rcut):
         self.cg_num = cg_num
         self.adjacency_matrix = adjacency_matrix
         self.CG_NN = CG_NN
-        self.cg_mapping = cg_mapping
+        #self.cg_mapping = cg_mapping
         self.rcut = rcut
         self.avg_cg_rdf = tf.keras.metrics.MeanTensor() # set up CG RDF tracking
 
@@ -228,9 +219,6 @@ class TrajModel(htf.SimModel):
     def compute(self, nlist, positions, box):
         # calculate the center of mass of a CG bead
         box_size = htf.box_size(box) # [16., 16., 16.]
-        mapped_pos = htf.center_of_mass(positions=positions[:,:3],
-                                        mapping=self.cg_mapping, 
-                                        box_size= box_size)
         cg_features = htf.compute_cg_graph(DSGPM=False,
                                        infile=None,
                                        adj_mat=self.adjacency_matrix,
@@ -243,7 +231,7 @@ class TrajModel(htf.SimModel):
         # because these are tensors, can't use list comprehension
         for i in range(len(cg_features[0])):
             cg_radius = htf.mol_bond_distance(CG=True,
-                                              cg_positions=mapped_pos,
+                                              cg_positions=positions,
                                               b1=cg_features[0][i][0],
                                               b2=cg_features[0][i][1]
                                               )
@@ -252,7 +240,7 @@ class TrajModel(htf.SimModel):
 
         for j in range(len(cg_features[1])):
             cg_angle = htf.mol_angle(CG=True,
-                                     cg_positions=mapped_pos,
+                                     cg_positions=positions,
                                      b1=cg_features[1][j][0],
                                      b2=cg_features[1][j][1],
                                      b3=cg_features[1][j][2]
@@ -262,7 +250,7 @@ class TrajModel(htf.SimModel):
 
         for k in range(len(cg_features[2])):
             cg_dihedral = htf.mol_dihedral(CG=True,
-                                           cg_positions=mapped_pos,
+                                           cg_positions=cg_features[-1],
                                            b1=cg_features[2][k][0],
                                            b2=cg_features[2][k][1],
                                            b3=cg_features[2][k][2],
@@ -272,138 +260,45 @@ class TrajModel(htf.SimModel):
         self.avg_cg_dihedrals.update_state(dihedrals_tensor)
 
         # create mapped neighbor list
-        mapped_nlist = htf.compute_nlist(mapped_pos, self.rcut, self.CG_NN, box_size, True)
+        mapped_nlist = nlist# htf.compute_nlist(mapped_pos, self.rcut, self.CG_NN, box_size, True)
         # compute RDF for mapped particles
         cg_rdf = htf.compute_rdf(mapped_nlist, [0.1, self.rcut])
         self.avg_cg_rdf.update_state(cg_rdf)
 
         # now calculate our total energy and train
-        nlist_r = htf.safe_norm(tensor=nlist[:, :, :3], axis=2)
+        nlist_r = htf.safe_norm(tensor=mapped_nlist[:, :, :3], axis=2)
         lj_energy = self.lj_energy(nlist_r)
         lj_energy_total = tf.reduce_sum(input_tensor=lj_energy, axis=1)
-        bonds_energy = self.bond_energy(radii_tensor)
-        angles_energy = self.angle_energy(angles_tensor)
-        dihedrals_energy = self.dihedral_energy(dihedrals_tensor)
-        subtotal_energy = tf.reduce_sum(bonds_energy) + tf.reduce_sum(angles_energy) + tf.reduce_sum(dihedrals_energy)
-        lj_forces = htf.compute_nlist_forces(nlist, lj_energy_total)
-        other_forces = htf.compute_positions_forces(positions=mapped_pos, energy=subtotal_energy)
-        total_energy = lj_energy + subtotal_energy
-        return lj_forces + other_forces, mapped_pos, total_energy, radii_tensor, angles_tensor, dihedrals_tensor
+        # bonds_energy = self.bond_energy(radii_tensor)
+        # angles_energy = self.angle_energy(angles_tensor)
+        # dihedrals_energy = self.dihedral_energy(dihedrals_tensor)
+        # subtotal_energy = tf.reduce_sum(bonds_energy) + tf.reduce_sum(angles_energy) + tf.reduce_sum(dihedrals_energy)
+        lj_forces = htf.compute_nlist_forces(mapped_nlist, lj_energy_total)
+        print(lj_forces.shape)
+        # other_forces = htf.compute_positions_forces(positions=mapped_pos, energy=subtotal_energy)
+        total_energy = lj_energy # + subtotal_energy
+        # return lj_forces + other_forces, mapped_pos, total_energy, radii_tensor, angles_tensor, dihedrals_tensor
+        return lj_forces, positions, total_energy, radii_tensor, angles_tensor, dihedrals_tensor 
 
 nneighbor_cutoff = 32
-model = TrajModel(nneighbor_cutoff,
+model = TrajModel(nneighbor_cutoff=nneighbor_cutoff,
                  cg_num=12, # beads per molecule, not total
                  adjacency_matrix=adjacency_matrix,
                  CG_NN=nneighbor_cutoff,
                  cg_mapping=cg_mapping,
+                 r_cut=set_rcut,
                  output_forces=False,
                  rcut=set_rcut,
                  check_nlist=False)
-    
-# for writing out the CG trajectory
-def make_frame(i, positions):
-    s = gsd.hoomd.Snapshot()
-    s.configuration.box = [16., 16., 16., 0., 0., 0.]
-    s.configuration.step = i
-    s.particles.N = beads_per_molecule * M
-    s.particles.position = positions
-    s.bonds.N = bonds_per_molecule * M
-    s.bonds.group = bonds_matrix
-    return s
-
-write_CG_traj = False
-
-avg_bond_lengths = []
-avg_bond_angles = []
-avg_dihedral_angles = []
-
-if write_CG_traj:
-    print(f'Applying CG mapping to {fname}')
-    f = gsd.hoomd.open(name=f'CG-traj-{fname}.gsd', mode='wb+')
-
-# get our potential energies from the log file
-logfile = f'{fname}.log'
-print(f'Reading energies from {logfile}')
-with open(logfile, 'r') as f:
-    header = f.readline()
-potential_energy_idx = header.split('\t').index('potential_energy')
-log_data = np.genfromtxt(logfile, skip_header=1)
-potential_energies = log_data[:, potential_energy_idx]
-energy_stride = 10 #int(log_data[:,0][1] - log_data[:,0][0])
-
-i = 0 # index over gsd timesteps
-j = 0 # index over energy steps (since we didn't write energy every frame)
 
 # all the 'None' here is so we only train on the energy
 model.compile('Adam', ['MeanSquaredError', None, None, None, None, None])
-losses = []
 
-# set up training data and get the raw mapped statistics
-inputs_list = []
-for inputs, ts in htf.iter_from_trajectory(nneighbor_cutoff, univ, r_cut=set_rcut):
-    if i % energy_stride == 0:
-        #labels = ts # TODO: iterate through the corresponding log file for our energies
-        # only grab neighbor list, positions, and box
-        inputs_list.append([np.array(item) for item in inputs])
-        result = model(inputs)
-        particle_positions = np.array(result[0])
-        avg_bond_lengths.append(result[2])
-        avg_bond_angles.append(result[3])
-        avg_dihedral_angles.append(result[4])
-        if write_CG_traj:
-            f.append(make_frame(i, particle_positions))
-        j += 1
-    i+=1
+system = simulate.System(molecule='PEEK', para_weight=1.0,
+                         density=1.2, n_compounds=[100],
+                         polymer_lengths=[4], forcefield='gaff',
+                         assert_dihedrals=True, remove_hydrogens=True)
 
-try:
-    np.save('inputs.npy', np.array(inputs_list))
-except:
-    print('failed to save inputs')
+sim = simulate.Simulation(system, gsd_write=100, mode='cpu', dt=0.0001, r_cut=set_rcut, tf_model=model)
 
-history = model.fit(x=inputs_list, y=potential_energies)# model.train_on_batch(x=inputs_list, y=potential_energies)
-
-cg_rdf = model.avg_cg_rdf.result().numpy()
-
-plt.figure()
-plt.plot(cg_rdf[1,:], cg_rdf[0,:], label='Mapped (CG)')
-plt.xlabel('r [$\AA$]')
-plt.ylabel('$g(r)$')
-plt.legend()
-plt.savefig('CG_RDF.svg')
-
-# plot average CG bond radii
-cg_radii = model.avg_cg_radii.result().numpy()
-
-np.save('cg_radii.npy', np.array(avg_bond_lengths))
-
-# plt.figure()
-# plt.hist(np.array(avg_bond_lengths))
-# plt.xlabel('r [$\AA$]')
-# plt.ylabel('Count')
-# plt.title('CG Bond Length Histogram')
-# plt.legend()
-# plt.savefig('CG_Radii.svg')
-
-# plot average CG bond angles
-# cg_radii = model.avg_cg_angles.result().numpy()
-
-np.save('cg_angles.npy', np.array(avg_bond_angles))
-
-# plt.figure()
-# plt.plot(cg_radii[1,:], cg_radii[0,:], label='Mapped (CG)')
-# plt.xlabel('$\theta$ [Degrees(?)]')
-# plt.ylabel('$g(r)$?')
-# plt.legend()
-# plt.savefig('CG_Angles.svg')
-
-# plot average CG dihedral angles
-# cg_radii = model.avg_cg_dihedrals.result().numpy()
-
-np.save('cg_dihedrals.npy', np.array(avg_dihedral_angles))
-
-# plt.figure()
-# plt.plot(cg_radii[1,:], cg_radii[0,:], label='Mapped (CG)')
-# plt.xlabel('$\phi$ [Degrees?]')
-# plt.ylabel('$g(r)$?')
-# plt.legend()
-# plt.savefig('CG_Radii.svg')
+sim.quench(kT=1., n_steps=1e6, shrink_steps=2e3)
